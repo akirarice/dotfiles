90e21b0d18f15b7a7f7d6218c819f0decfa056be
akn <akira.nakano.cbe@gmail.com>
2021-06-19 21:08:30 +0200

    update with shellchecker audit

--- .local/bin/bookmarker.sh
+++ .local/bin/bookmarker.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 bookmark=$(xclip -o -selection p)
-title=$(wget -qO- $bookmark | grep -o -P '(?<=<title>).*(?=</title>)')
+title=$(wget -qO- "$bookmark" | grep -o -P '(?<=<title>).*(?=</title>)')
 
 [ -z "$title" ] && notify-send "url title not found" 
 curl -s --head --request --fail "$bookmark" || notify-send "warning: url not valid" 
 
-if grep -qx "$bookmark" $BOOKMARKS/bookmarks; then
+if grep -qx "$bookmark" "$BOOKMARKS"/bookmarks; then
 	notify-send "'$bookmark' already in bookmarks"
 else
 	request=$(printf "Yes\\nNo" | dmenu -h 40 -i -p "Add $bookmark to bookmarks?") || exit
-                [ "$request" = "Yes" ] && echo "$bookmark" >> $BOOKMARKS/bookmarks 
-		[ -z "$title" ] && echo "$bookmark" >> $BOOKMARKS/bookmark_titles &&
+                [ "$request" = "Yes" ] && echo "$bookmark" >> "$BOOKMARKS"/bookmarks 
+		[ -z "$title" ] && echo "$bookmark" >> "$BOOKMARKS"/bookmark_titles &&
 			notify-send "$bookmark added to bookmarks" || 
-			echo "$title" >> $BOOKMARKS/bookmark_titles && 
+			echo "$title" >> "$BOOKMARKS"/bookmark_titles && 
 			notify-send "$title added to bookmarks"
 fi
